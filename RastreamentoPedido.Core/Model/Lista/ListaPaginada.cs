876f06e65c8af0e71a0d93899618709edd8a73a7
PedroLuc-cpu <pedrolucas_gta2015@hotmail.com>
2025-05-10 10:28:07 -0300

    feat: criado uma class para ListaPaginada

---
+++ RastreamentoPedido.Core/Model/Lista/ListaPaginada.cs
@@ -0,0 +1,28 @@
+﻿
+namespace RastreamentoPedido.Core.Model.Lista
+{
+    internal class ListaPaginada<T>
+    {
+        public int PaginaAtual { get; set; }
+        public int TotalPaginas { get; set; }
+
+        public List<T> Itens { get; set; }
+
+        public ListaPaginada(List<T> itens, int count, int pageIndex, int pageSize)
+        {
+            PaginaAtual = pageIndex;
+            TotalPaginas = (int)Math.Ceiling(count / (double)pageSize);
+            Itens = new List<T>();
+            Itens.Clear();
+            Itens.AddRange(itens);
+        }
+
+        public ListaPaginada()
+        {
+            PaginaAtual = 1;
+            TotalPaginas = 15;
+            Itens = new List<T>();
+        }
+
+    }
+}
