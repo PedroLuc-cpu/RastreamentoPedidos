ea0b16a0df8a0584c9a8c94bcd697530c6d64622
PedroLuc-cpu <pedrolucas_gta2015@hotmail.com>
2025-07-03 15:12:28 -0300

    feat(auth): adicionar suporte à autenticação com Google
    - Adicionado o pacote `Microsoft.AspNetCore.Authentication.Google` ao projeto.
    
    - Configurados os serviços de autenticação no `Program.cs` para utilizar autenticação via cookies e autenticação com Google.
    
    - Especificados `clientId`, `clientSecret` e escopos necessários para integração com o Google.

--- RastreamentoPedidos/Program.cs
+++ RastreamentoPedidos/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using RastreamentoPedido.Core.Helpers;
 using RastreamentoPedidos.API.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(options => 
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.SaveTokens = true;
+        options.Scope.Add("email");
+        options.Scope.Add("profile");
+        options.Scope.Add("openid");
+    });
 builder.Configuration.SetDefaultConfiguration(builder.Environment);
 builder.Services.AddIdentityConfiguration(builder.Configuration);
 builder.Services.AddApiConfiguration(builder.Configuration);
